09d58b76a869cc45b9b8bb9fb6b70a4000188a16
Paloma Tamayo <p.tamayo.segura@gmail.com>
2025-06-14 21:45:58 -0400

    actualización perfil

--- FRONTEND/src/app/constantes.ts
+++ FRONTEND/src/app/constantes.ts
@@ -2,7 +2,7 @@
 export class Constantes{
 
     public static get API_URL(): string {
-       // return "http://localhost:8080";
-       return "https://vecindapptodo-production.up.railway.app";
+        return "http://localhost:8080";
+       //return "https://vecindapptodo-production.up.railway.app";
     }
 }
