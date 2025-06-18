e8ac2dad626968870c147cb51256df105495d61e
Paloma Tamayo <p.tamayo.segura@gmail.com>
2025-06-18 11:44:23 -0400

    cambio final constantes

--- FRONTEND/src/app/constantes.ts
+++ FRONTEND/src/app/constantes.ts
@@ -1,5 +1,6 @@
 import { environment } from '../environments/environment';
 
+export class Constantes {
     public static get API_URL(): string {
         return "http://localhost:8080";
        //return "https://vecindapptodo-production.up.railway.app";
